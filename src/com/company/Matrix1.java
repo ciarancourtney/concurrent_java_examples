78eaf951d7afbd516d5de5c75d2b8d08ee072fff
ciarancourtney <ciaran.courtney@cylon.com>
2017-02-20 23:43:33 +0000

    Matrix1: Generate A & B matrices randomally in range 0 > 10

--- src/com/company/Matrix1.java
+++ src/com/company/Matrix1.java
@@ -5,33 +5,38 @@ public class Matrix1
 {
     public static void main(String[] args)
     {
+        // define matrix size
+        int size = 100;
 
-        int [] [] A = {
-            {20, 18, 22, 20},
-            {18, 20, 18, 21},
-            {16, 18, 16, 20},
-            {25, 24, 22, 24}
-        };
+        // Create random matrix A of size X size
+        int [][] A = new int[size][size];
+        for (int i=0; i<size; i++) {
+            for (int j=0; j<size; j++) {
+                A[i][j] = (int) (Math.random()*10);
+            }
+        }
 
-        int [] [] B = {
-            {20, 18, 22, 20},
-            {18, 20, 18, 21},
-            {16, 18, 16, 20},
-            {25, 24, 22, 24}
-        };
+        // Create random matrix B of size X size
+        int [][] B = new int[size][size];
+        for (int i=0; i<size; i++) {
+            for (int j=0; j<size; j++) {
+                B[i][j] = (int) (Math.random()*10);
+            }
+        }
 
-        int shape = A.length;  // assume [NxN].[NxN]
+        // init a zero matrix C of size X size
+        int[][] C = new int[size][size];
 
-        int[][] C = new int[shape][shape];  // init a C = NxN matrix of zeros
-
-        for (int i = 0; i < shape; i++) {
-            for (int j = 0; j < shape; j++) {
-                for (int k = 0; k < shape; k++) {
+        // Calculate using 4 for loops the result of A.B = C
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < size; k++) {
                     C[i][j] += A[i][k] * B[k][j];
                 }
             }
         }
 
+        // print C matrix row by row
         for (int i = 0; i < C.length; i++) {
             for (int j = 0; j < C[i].length; j++) {
                 System.out.print(C[i][j] + " ");
