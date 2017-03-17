2a15abffd875509163c7f709d6b5cf0e8f15c236
ciarancourtney <ciaran.courtney@cylon.com>
2017-03-17 00:38:43 +0000

    Add Matrix43b which uses column-heavy 8-way split

---
+++ src/com/company/Matrix43b.java
@@ -0,0 +1,119 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+
+public class Matrix43b {
+
+    public static void main(String args[]) {
+        // define matrix size
+        int size = Integer.parseInt(args[0]);
+        int thread_pool = Integer.parseInt(args[1]);
+
+        // Create random matrix A and B of shape <size> by <size>
+        int[][] A = new int[size][size];
+        int[][] B = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                A[i][j] = (int) (Math.random() * 10);
+                B[i][j] = (int) (Math.random() * 10);
+            }
+        }
+
+        // Create an empty matrix C of shape <size> by <size>
+        int[][] C = new int[size][size];
+
+        int row_unit = size/4;
+        final long startTime = System.currentTimeMillis();
+        // execute thread for each section of matrix N.B. this code assumes an even length square matrix
+        ExecutorService threadExecutor = Executors.newFixedThreadPool(thread_pool);
+        threadExecutor.execute(new Thread(new WorkerThread43b(0, row_unit-1, 0, (size/2)-1, A, B, C, size)));  // Left Column 1 
+        threadExecutor.execute(new Thread(new WorkerThread43b(row_unit, row_unit*2-1, 0, (size/2)-1, A, B, C, size)));  // Left Column 2
+        threadExecutor.execute(new Thread(new WorkerThread43b(row_unit*2, row_unit*3-1, 0, (size/2)-1, A, B, C, size)));  // Left Column 3
+        threadExecutor.execute(new Thread(new WorkerThread43b(row_unit*3, size-1, 0, (size/2)-1, A, B, C, size)));  // Left Column 4
+        
+        threadExecutor.execute(new Thread(new WorkerThread43b(0, row_unit-1, size/2, size-1, A, B, C, size)));  // Right Column 1
+        threadExecutor.execute(new Thread(new WorkerThread43b(row_unit, row_unit*2-1, size/2, size-1, A, B, C, size)));  // Right Column 2
+        threadExecutor.execute(new Thread(new WorkerThread43b(row_unit*2, row_unit*3-1, size/2, size-1, A, B, C, size)));  // Right Column 3
+        threadExecutor.execute(new Thread(new WorkerThread43b(row_unit*3, size-1, size/2, size-1, A, B, C, size)));  // Right Column 4
+        threadExecutor.shutdown();
+
+        try {
+            threadExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("threadExecutor.awaitTermination caught exception");
+        }
+
+        final long endTime = System.currentTimeMillis();
+        long duration = (endTime - startTime);
+        System.out.println("Total Time: " + duration + " miliseconds");
+        System.out.println("Total Enumerated Thread Count: " + thread_pool + " ");
+
+
+        // confirm calc by calculating C using known algorithm (pure row ikj) and comparing
+        int[][] naiveC = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            int[] arowi = A[i];
+            int[] crowi = naiveC[i];
+            for (int k = 0; k < size; k++) {
+                int[] browk = B[k];
+                int aik = arowi[k];
+                for (int j = 0; j < size; j++) {
+                    crowi[j] += aik * browk[j];
+                }
+            }
+        }
+        if (Arrays.deepEquals(naiveC, C)) {
+           System.out.println("A.B is correct");
+        } else {
+           System.out.println("A.B is not correct");
+        }
+
+    }
+}
+
+
+class WorkerThread43b implements Runnable {
+    private int start_row;
+    private int end_row;
+    private int start_col;
+    private int end_col;
+    private int A[][];
+    private int B[][];
+    private int C[][];
+    private int size;
+
+    WorkerThread43b(int start_row, int end_row, int start_col, int end_col, int A[][], int B[][], int C[][], int size) {
+        this.start_row = start_row;
+        this.end_row = end_row;
+        this.start_col = start_col;
+        this.end_col = end_col;
+        this.A = A;
+        this.B = B;
+        this.C = C;
+        this.size = size;
+    }
+
+    @Override
+    public void run() {
+        int i = start_row;
+        int j = start_col;
+
+        //System.out.println("[(" + start_row + ", " + end_row + "), (" + start_col + ", " + end_col + ")]");
+        for (i = start_row; i <= end_row; i++) {
+            int[] arowi = A[i];
+            int[] crowi = C[i];
+            for (int k = 0; k < size; k++) {
+                int[] browk = B[k];
+                int aik = arowi[k];
+                for (j = start_col; j <= end_col; j++) {
+                    crowi[j] += aik * browk[j];
+                }
+            }
+        }
+    }
+}
